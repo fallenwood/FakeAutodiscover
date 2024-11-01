764be3ac23e1bcb219d75d1dcf25950bb57ac2d7
Fallenwood <fallenwood.y@outlook.com>
2024-11-01 22:11:02 +0800

    init

---
+++ Program.cs
@@ -0,0 +1,54 @@
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateSlimBuilder(args);
+
+builder.Services.ConfigureHttpJsonOptions(options =>
+{
+    options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
+});
+
+var app = builder.Build();
+
+app.MapGet(
+    "/autodiscover/autodiscover.json",
+    async (HttpContext context, [FromQuery(Name = "Email")] string? email, [FromQuery(Name = "Protocol")] string? protocol, [FromServices] ILogger<Program> logger) =>
+{
+    var socketIp = context.Connection.RemoteIpAddress?.ToString();
+    var headerIp = context.Request.Headers["X-Forwarded-For"];
+
+    var ipAddress = socketIp;
+    if (string.IsNullOrEmpty(ipAddress))
+    {
+        ipAddress = headerIp.ToString();
+    }
+
+    logger.LogInformation("Incoming request from '{IPAddress}' with email '{Email}' and protocl '{Protocol}'.", ipAddress, email, protocol);
+
+    context.Response.StatusCode = 200;
+    context.Response.Headers.Append("X-Client-IPAddress", ipAddress);
+
+    await context.Response.WriteAsJsonAsync<ActiveSyncResponse>(new());
+});
+
+
+
+await app.RunAsync();
+
+
+public sealed class ActiveSyncResponse(
+    string protocol = "ActiveSync",
+    string url = "https://outlook.office365.com/Microsoft-Server-ActiveSync")
+{
+    [JsonPropertyName("Protocol")]
+    public string Protocl { get; init; } = protocol;
+
+    [JsonPropertyName("Url")]
+    public string Url { get; init; } = url;
+}
+
+[JsonSerializable(typeof(ActiveSyncResponse))]
+internal partial class AppJsonSerializerContext : JsonSerializerContext
+{
+
+}
